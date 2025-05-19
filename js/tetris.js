fe3ca19b285979b73315d4ca460c48fe54cba0a7
matsumoto0123 <241205163@ccmailg.meijo-u.ac.jp>
2025-05-19 14:25:54 +0900

    色を変更

--- js/tetris.js
+++ js/tetris.js
@@ -22,7 +22,7 @@ var shapes = [
       1, 1, 1 ]
 ];
 var colors = [
-    'tan', 'tan', 'tan', 'tan', 'tan', 'tan', 'tan'
+    'tan', 'tan', 'skyblue', 'tan', 'tan', 'tan', 'tan'
 ];
 
 // creates a new 4x4 shape in global variable 'current'
